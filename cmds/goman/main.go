e5ea7d56f4208d9854e4f1ecc62582d6fe59d2c3
John Berthels <john.berthels@gmail.com>
2023-03-20 17:49:30 +0000

    initial import - basic animation

---
+++ cmds/goman/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+)
+
+func main() {
+	a := app.New()
+
+	title := "Go, Man"
+	w := 640
+	h := 480
+
+	win := a.NewWindow(title)
+
+	img := makeImage(w, h)
+
+	tickDur := 10 * time.Millisecond
+	ticker := time.NewTicker(tickDur)
+
+	fyneImg := canvas.NewImageFromImage(img)
+	win.SetContent(fyneImg)
+	win.Resize(fyne.NewSize(float32(w), float32(h)))
+
+	animateImage := func(img *image.RGBA, w, h int, ch <-chan time.Time) {
+		tick := 0
+		maxTick := w / 2
+
+		for {
+			generateImage(img, w, h, tick)
+			canvas.Refresh(fyneImg)
+			tick++
+			tick = tick % maxTick
+			<-ch
+		}
+	}
+	go animateImage(img, w, h, ticker.C)
+
+	win.ShowAndRun()
+}
+
+func makeImage(w, h int) *image.RGBA {
+	img := image.NewRGBA(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{w, h}})
+	return img
+}
+
+func generateImage(img *image.RGBA, w, h, tick int) {
+	draw.Draw(img, img.Bounds(), image.Black, image.ZP, draw.Src)
+	x2 := w / 2
+	y2 := h / 2
+	c := color.RGBA{255, 0, 0, 255}
+	offset := tick - w/4
+	for i := -w / 4; i < w/4; i++ {
+		img.Set(x2+i+offset, y2+i, c)
+	}
+}
