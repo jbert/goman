1e93678867b57e3863288ae98c9640042bc7866a
John Berthels <john.berthels@gmail.com>
2023-03-24 11:29:04 +0000

    pull out mandel struct, don't pass undeeded width+height

--- cmds/goman/main.go
+++ cmds/goman/main.go
@@ -36,8 +36,8 @@ func main() {
 		maxTick := w / 2
 
 		for {
-			clearImage(img, w, h)
-			generateImage(img, w, h, tick)
+			clearImage(img)
+			generateImage(img, tick)
 			canvas.Refresh(fyneImg)
 			tick++
 			tick = tick % maxTick
@@ -50,17 +50,25 @@ func main() {
 	win.ShowAndRun()
 }
 
+func widthHeight(img image.Image) (int, int) {
+	bounds := img.Bounds()
+	w := bounds.Max.X - bounds.Min.X
+	h := bounds.Max.Y - bounds.Min.Y
+	return w, h
+}
+
 func makeImage(w, h int) *image.RGBA {
 	img := image.NewRGBA(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{w, h}})
 	return img
 }
 
-func clearImage(img *image.RGBA, w, h int) {
+func clearImage(img *image.RGBA) {
 	draw.Draw(img, img.Bounds(), image.Black, image.ZP, draw.Src)
 }
 
-func generateImage(img *image.RGBA, w, h, tick int) {
-	drawMandelbrot(img, w, h, tick)
+func generateImage(img *image.RGBA, tick int) {
+	m := NewMandel()
+	m.Draw(img)
 	/*
 		x2 := w / 2
 		y2 := h / 2
@@ -72,21 +80,18 @@ func generateImage(img *image.RGBA, w, h, tick int) {
 	*/
 }
 
-func drawMandelbrot(img *image.RGBA, w, h, tick int) {
-	fmt.Printf("%s: draw %d\n", time.Now(), tick)
-	steps := 100
-	xlo := -2.5
-	xhi := 1.0
-	xw := xhi - xlo
-	ylo := -1.5
-	yhi := 1.5
-	yw := yhi - ylo
+func (m *Mandel) Draw(img *image.RGBA) {
+	w, h := widthHeight(img)
+	fmt.Printf("%s: draw %d\n", time.Now())
+
+	xw := m.Xhi - m.Xlo
+	yw := m.Yhi - m.Ylo
 
 	for i := 0; i < w; i++ {
 		for j := 0; j < h; j++ {
-			x := xlo + (xw * float64(i) / float64(w))
-			y := ylo + (yw * float64(j) / float64(h))
-			mag := mandelCalc(x, y, steps)
+			x := m.Xlo + (xw * float64(i) / float64(w))
+			y := m.Ylo + (yw * float64(j) / float64(h))
+			mag := m.calcPoint(x, y)
 			//			fmt.Printf("x %v y %v mag %v\n", x, y, mag)
 			c := color.RGBA{0, 0, 0, 255}
 			if mag < 1.0 {
@@ -97,17 +102,34 @@ func drawMandelbrot(img *image.RGBA, w, h, tick int) {
 	}
 }
 
-func mandelCalc(x, y float64, steps int) float64 {
-	threshold := 1000.0
+type Mandel struct {
+	Steps     int
+	Xlo, Xhi  float64
+	Ylo, Yhi  float64
+	Threshold float64
+}
 
+func NewMandel() *Mandel {
+	return &Mandel{
+		Steps:     100,
+		Xlo:       -2.5,
+		Xhi:       1.0,
+		Ylo:       -1.5,
+		Yhi:       1.5,
+		Threshold: 1000,
+	}
+}
+
+func (m *Mandel) calcPoint(x, y float64) float64 {
 	c := complex(x, y)
 
 	z := complex(0, 0)
+	steps := m.Steps
 	for steps > 0 {
 		//		fmt.Printf("z %v c %v\n", z, c)
 		z = z*z + c
-		if cmplx.Abs(z) > threshold {
-			return threshold
+		if cmplx.Abs(z) > m.Threshold {
+			return m.Threshold
 		}
 		steps--
 	}
